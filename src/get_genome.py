35e812fb94758a276c0127e53d34c4e78d005e44
exenale <exenale@gmail.com>
2022-02-15 22:03:51 -0500

    Filling out funtions to fetch and create genome obj

--- src/get_genome.py
+++ src/get_genome.py
@@ -1,6 +1,8 @@
 """Accepts a genome ID, fetches it from NCBI and pushes it to the db + file storage"""
 import sys
 from dataclasses import dataclass
+from Bio import Entrez, SeqIO
+ 
 
 @dataclass
 class OrganismGenome:
@@ -9,9 +11,14 @@ class OrganismGenome:
     file_path: str
     file_contents: str
 
-def get_genome_from_ncbi(genome_id: str) -> OrganismGenome:
-    pass
+def get_genome_from_ncbi(genome_id: str):
+    Entrez.email =  "admin@metainformant.com" #parameterize?
+    handle = Entrez.efetch(db="nuccore", id=genome_id, rettype="gb", retmode="text")
+    record = SeqIO.read(handle, "fasta")
+    return record
 
+def check_db_if_exists(genome_id: str) -> (None | OrganismGenome):
+    return None
 
 def upload_file_to_local_dir(OrganismGenome) -> bool:
     pass
@@ -21,3 +28,12 @@ def upload_file_to_gcs(OrganismGenome) -> bool:
 
 def add_record_to_db(OrganismGenome, postgresdb) -> bool:
     pass
+
+def get_genome(genome_id) -> OrganismGenome:
+    # check db first, if exists fetch object
+    genome_obj = check_db_if_exists(genome_id)
+
+    if not genome_obj:
+        pass
+        # otherwise generate new object
+    return genome_obj
